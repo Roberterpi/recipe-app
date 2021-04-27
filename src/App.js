2ae49f34093343b20421ff0b9441d43a60288087
roberterpi <robertpopct@gmail.com>
2021-04-27 17:47:58 +0300

    added api id and key

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello React</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  
+  const APP_ID = "2ee0676b";
+  const APP_KEY = "1ce518b3cfff59137985efdb07ee9cfc";
+
+  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
+
+  return(
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+};
 
 export default App;

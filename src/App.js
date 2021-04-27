8207e033560db64fbc4c201b47aa2d3c34473e9c
roberterpi <robertpopct@gmail.com>
2021-04-27 19:24:17 +0300

    removed h1

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ const App = () => {
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)} >{counter}</h1>
     </div>
   );
 };

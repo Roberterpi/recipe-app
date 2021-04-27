2e1130479f88f1b05c7384005c95cdca6e301933
roberterpi <robertpopct@gmail.com>
2021-04-27 21:22:22 +0300

    added key in recipe in apps.js, added const search

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   const APP_KEY = "1ce518b3cfff59137985efdb07ee9cfc";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     getRecipes();
@@ -17,16 +18,22 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-  }
+    console.log(data.hits);
+  };
 
-  return(
+  return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map(recipe => (
-        <Recipe />
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        />
       ))}
     </div>
   );

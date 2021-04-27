94f1fca3b3db92b9fb6f7f1bc869b75340a1e0cd
roberterpi <robertpopct@gmail.com>
2021-04-27 17:59:03 +0300

    added classes into form, input and button

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ const App = () => {
 
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-form" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 };

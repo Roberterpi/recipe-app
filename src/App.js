84cba15f735047eda026dcbacf304dd152652115
roberterpi <robertpopct@gmail.com>
2021-04-27 17:50:59 +0300

    updated app id and app key in url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const APP_ID = "2ee0676b";
   const APP_KEY = "1ce518b3cfff59137985efdb07ee9cfc";
 
-  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}}&app_key=${APP_KEY}`
 
   return(
     <div className="App">
